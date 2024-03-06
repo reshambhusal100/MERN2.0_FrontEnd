5b45aee71508962ce294f930ecb6a1a153ac0413
unknown <Resham Bhusal>
2024-03-06 20:51:13 +0545

    changing

--- src/pages/singleBook/SingleBook.jsx
+++ src/pages/singleBook/SingleBook.jsx
@@ -7,7 +7,7 @@ const SingleBook = () => {
   const {id} = useParams()
   const [book,setBook] = useState({})
   const fetchBook = async ()=>{
-    const response = await axios.get(`http://localhost:3000/book/${id}`) 
+    const response = await axios.get(`https://mern2-0-backend.onrender.com/book/${id}`) 
     if(response.status === 200){
       setBook(response.data.data)
     }
