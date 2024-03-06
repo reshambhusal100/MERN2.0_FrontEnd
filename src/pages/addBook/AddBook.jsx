60d2b5d15787539e0a4bf68989ff0bbe321a12ce
unknown <Resham Bhusal>
2024-03-06 20:39:33 +0545

    backen URL changed

--- src/pages/addBook/AddBook.jsx
+++ src/pages/addBook/AddBook.jsx
@@ -53,7 +53,7 @@ const handleSubmit = async (e)=>{
   })
   formData.append('image',image)
 
-  const response = await axios.post("http://localhost:3000/book",formData)
+  const response = await axios.post("https://mern2-0-backend.onrender.com/book",formData)
   if(response.status === 201){
     navigate("/")
   }else{
